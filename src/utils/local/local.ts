5048a4acc08697bd447ef6f4adc1ce3b96803953
Nagy Viktor <nagy.viktor@fotex.net>
2023-04-26 11:39:32 +0200

    feat: added options for local utility

--- src/utils/local/local.ts
+++ src/utils/local/local.ts
@@ -3,17 +3,22 @@ import { v4 as uuid } from 'uuid';
 import IResponse from '../../interfaces/IResponse';
 
 export type Credentials = { email: string; password: string };
+export type LoginOptions = { cacheUrl?: string };
+
 type DatabaseRecord = { id: number } & Record<string, unknown>;
 type AuthResponse<TUser extends DatabaseRecord> = IResponse<{ [x: string]: TUser }>;
 type User<TProps extends DatabaseRecord> = { token: string } & TProps;
 
 function local<TRecord extends DatabaseRecord = DatabaseRecord>(apiUrl: string, credentials: Credentials) {
-  return async (key: string): Promise<User<TRecord> | null> => {
+  return async (key: string, options?: LoginOptions): Promise<User<TRecord> | null> => {
     try {
       const { data, headers } = await axios.post<AuthResponse<TRecord>>(apiUrl, credentials, { withCredentials: true });
       const cacheId = createCache();
       const user = data.data[key];
       const token = headers.authorization?.split(' ')[1];
+
+      if (!!options?.cacheUrl) axios.post(options?.cacheUrl, { [key]: user, token });
+
       return { ...user, token, cacheId };
     } catch (error) {
       return null;
