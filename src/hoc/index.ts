5588b09c29755629d509ca4a57e931cfb733b704
Nagy Viktor <nagy.viktor@fotex.net>
2023-05-04 15:44:10 +0200

    chore: added exports

---
+++ src/hoc/index.ts
@@ -0,0 +1,1 @@
+export { default as withAuthGuard, AuthGuardConfig } from './withAuthGuard/withAuthGuard';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 // export everything here..
+export * from './hoc';
 export * from './utils';
