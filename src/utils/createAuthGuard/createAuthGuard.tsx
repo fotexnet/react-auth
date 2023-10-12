6d3defaf0bc8cbd1e53c6415f02f5b2135cf76e8
Nagy Viktor <nagy.viktor@fotex.net>
2023-10-12 10:03:23 +0200

    fix(build issue): build issue

--- src/utils/createAuthGuard/createAuthGuard.tsx
+++ src/utils/createAuthGuard/createAuthGuard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import withAuthGuard, { AuthGuardConfig } from '../../hoc/withAuthGuard/withAuthGuard';
+import { DefaultUser } from '../createUserProvider/types';
 
-function createAuthGuard<T extends object>(config: AuthGuardConfig) {
+function createAuthGuard<U extends DefaultUser, T extends object>(config: AuthGuardConfig<U>) {
   return (Component: React.ComponentType<T>): React.FC<T> => {
     return withAuthGuard(Component, config);
   };
